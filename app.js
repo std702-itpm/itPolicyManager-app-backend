431a157244c6b408850d8842b3de35f6364ff97d
Anton Rozhanskii <anton_rozhanskii@yahoo.com>
2020-05-24 21:13:05 +1200

    Move mongoose configuration into separate file
    For better readability.

--- app.js
+++ app.js
@@ -1,43 +1,24 @@
 const express = require('express');
+const mongoose = require('./configs/mongoose.config');
+var session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
 const cors = require('cors');
-const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-var session = require('express-session');
-var MongoDBStore = require('connect-mongodb-session')(session);
-
-// invoke express on port 5000
 const app = express();
-const port = process.env.PORT || 5000;
-
-var store = new MongoDBStore({
-    uri: process.env.DATABASE_URL,
-    collection: 'sessions',
-    unset: 'destroy'
-});
-
-store.on('error', function (error) {
-    console.log(error);
-});
 
 app.use(session({
     secret: 'Somesercrets',
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 1 // 1 day
     },
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    }),
     resave: true,
     saveUninitialized: false
 }));
-
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
-const connection = mongoose.connection;
-
-connection.once('open', function () {
-    console.log("MongoDB database connection established successfully");
-})
 
 app.use(cors());
 app.use(express.json());
@@ -46,6 +27,8 @@ app.use(express.json());
 const indexRoute = require('./routes');
 app.use('/', indexRoute);
 
+// invoke express on assigned or 5000 port
+const port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log("Server is running on Port: " + port);
 });
