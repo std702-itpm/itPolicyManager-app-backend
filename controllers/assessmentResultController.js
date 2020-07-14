9065f644ca6bc5d4644c7f852f7229696a8132a3
Pangseen Puarat <boomer_saw@hotmail.com>
2020-07-14 18:24:37 +1200

    Feature/save assessment result (#20)
    * Fixed indentation
    
    * Refactored assessment result save function.
    
    Co-authored-by: Pangseen Puarat <pangseen.puarat@xencio.at>

--- controllers/assessmentResultController.js
+++ controllers/assessmentResultController.js
@@ -1,23 +1,27 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("../models/assessmentResult.model.js");
-const AssessmentResult=mongoose.model("AssessmentResult");
+const AssessmentResult = mongoose.model("AssessmentResult");
 
 //Save the score from assessment result
-exports.PostResult=(req,res)=>{
-   var data=req.body;
-   var result=new AssessmentResult({
-        score:data.score,
-        assessment_taken_date:Date.now(),
-        status:true.query,
-        reviewer_id:data.reviewerId,
-
-   })
-   result.save(function(err,res){
-       if(!err){
-           console.log(res)
-       }
-       else{
-           console.log(err)
-       }
-   });
+exports.PostResult = (req, res) => {
+    let data = req.body;
+    let assessmentResult = new AssessmentResult({
+        score: data.score,
+        max_score: data.maxScore,
+        assessment_taken_date: Date.now(),
+        is_accep_compliance: data.isComplianceChecked,
+        assessment_taker: data.user,
+    })
+    assessmentResult.save(function (err, document) {
+        if (err) {
+            res.status(500)
+                .json(error);
+            console.log(error)
+        }
+        console.log(document)
+        res.json({
+            message: "Your assessment has been saved successfully.",
+            result: document
+        })
+    });
 };
--- models/assessmentResult.model.js
+++ models/assessmentResult.model.js
@@ -1,11 +1,15 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const assessmentResultSchema=new Schema({
-    assessment_taken_date:Date,
-    score:Number,
-    status:Boolean,
-    reviewer_id:Schema.Types.ObjectId
+const assessmentResultSchema = new Schema({
+    assessment_taken_date: Date,
+    score: Number,
+    max_score: Number,
+    is_accep_compliance: Boolean,
+    assessment_taker: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 module.exports = mongoose.model("AssessmentResult", assessmentResultSchema);
